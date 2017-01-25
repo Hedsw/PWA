dc091ea98910e73805b75f4dc74f4e3eeabd3a83
Yunhyeok Lee <yunhyeoklee@Yunhyeoks-MacBook-Pro.local>
2017-01-24 21:02:18 -0600

    Changed Service Workers parts

--- step-05/service-worker.js
+++ step-05/service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('install', function(e) {
       return cache.addAll(filesToCache);
     })
   );
-});
+})
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
@@ -41,18 +41,12 @@ self.addEventListener('activate', function(e) {
       }));
     })
   );
-  /*
-   * Fixes a corner case in which the app wasn't returning the latest data.
-   * You can reproduce the corner case by commenting out the line below and
-   * then doing the following steps: 1) load app for first time so that the
-   * initial New York City data is shown 2) press the refresh button on the
-   * app 3) go offline 4) reload the app. You expect to see the newer NYC
-   * data, but you actually see the initial data. This happens because the
-   * service worker is not yet activated. The code below essentially lets
-   * you activate the service worker faster.
-   */
   return self.clients.claim();
-});
+})
+
+self.addEventListener('fetch', function(event) {
+  // Do something interesting with the fetch here
+})
 
 self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
